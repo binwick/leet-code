92e08e3dc561b7c77250fbc08ff715c2b5c68b4a
liuruibin <bin@fit2cloud.com>
2019-07-25 21:55:02 +0800

    43

--- lc_0043/multipy.go
+++ lc_0043/multipy.go
@@ -17,15 +17,19 @@ func multiply(num1 string, num2 string) string {
 		for j := l2 - 1; j >= 0; j-- {
 			ni := int(num1[i] - '0') // '0' = 48
 			nj := int(num2[j] - '0')
-			a[i+j] += ni * nj
-			if a[i+j] >= 10 && (i+j) != 0 {
-				a[i+j-1] += a[i+j] / 10
-				a[i+j] = a[i+j] % 10
+			a[i+j+1] += ni * nj
+			if a[i+j+1] >= 10 {
+				a[i+j] += a[i+j+1] / 10
+				a[i+j+1] = a[i+j+1] % 10
 			}
 		}
 	}
-	for i := 0; i <= l1+l2-2; i++ {
+	for i := 0; i < l1+l2; i++ {
 		s += strconv.Itoa(a[i])
+	}
+
+	if s[0] == '0' {
+		return s[1:]
 	}
 
 	return s
@@ -33,7 +37,7 @@ func multiply(num1 string, num2 string) string {
 
 func main() {
 
-	s := multiply("123", "456")
+	s := multiply("999", "999")
 	s = multiply("498828660196", "840477629533")
 	fmt.Println(s)
 }
