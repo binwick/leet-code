1d5e4e8dceec6d32f592bb8c9f507fa00bd090af
binwick <binwick@gmail.com>
2019-07-25 19:28:30 +0800

    0043

--- lc_0043/multipy.go
+++ lc_0043/multipy.go
@@ -2,29 +2,38 @@ package main
 
 import (
 	"fmt"
-	"math"
 	"strconv"
 )
 
 func multiply(num1 string, num2 string) string {
-	var sum int64
+	var s string
 	for i := len(num1) - 1; i >= 0; i-- {
 		var next int
 		for j := len(num2) - 1; j >= 0; j-- {
-			ni, _ := strconv.Atoi(string(num1[i]))
-			nj, _ := strconv.Atoi(string(num2[j]))
+			ni := int(num1[i] - 48)
+			nj := int(num2[j] - 48)
 			tmp := ni*nj + next
-			p := math.Pow10(len(num1) - 1 - i + len(num2) - 1 - j)
-			tmp = tmp * int(p)
-			sum += int64(tmp)
+			if i == len(num1)-1 {
+				s = strconv.Itoa(tmp%10) + s
+				next = tmp / 10
+			} else {
+				sum := int(s[j]-48) + tmp%10
+				next = sum/10 + tmp/10
+				s = s[0:j] + strconv.Itoa(sum%10) + s[j+1:]
+			}
 		}
+		s = strconv.Itoa(next) + s
 	}
-	fmt.Println(sum)
-	return ""
+
+	if s[0] == 48 {
+		return s[1:]
+	}
+	return s
 }
 
 func main() {
 
 	s := multiply("123", "456")
+	s = multiply("498828660196", "840477629533")
 	fmt.Println(s)
 }
