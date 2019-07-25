f4728d81a5c9cfff4911681941411aa7b66d9d71
liuruibin <bin@fit2cloud.com>
2019-07-25 21:57:23 +0800

    43

--- lc_0043/multipy.go
+++ lc_0043/multipy.go
@@ -17,10 +17,11 @@ func multiply(num1 string, num2 string) string {
 		for j := l2 - 1; j >= 0; j-- {
 			ni := int(num1[i] - '0') // '0' = 48
 			nj := int(num2[j] - '0')
-			a[i+j+1] += ni * nj
-			if a[i+j+1] >= 10 {
-				a[i+j] += a[i+j+1] / 10
-				a[i+j+1] = a[i+j+1] % 10
+			index := i + j + 1
+			a[index] += ni * nj
+			if a[index] >= 10 {
+				a[index-1] += a[index] / 10 //进位
+				a[index] = a[index] % 10
 			}
 		}
 	}
@@ -38,6 +39,6 @@ func multiply(num1 string, num2 string) string {
 func main() {
 
 	s := multiply("999", "999")
-	s = multiply("498828660196", "840477629533")
+	//s = multiply("498828660196", "840477629533")
 	fmt.Println(s)
 }
