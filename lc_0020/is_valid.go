b3448a2bbed4bc7b4609040a4c5df6ccad067d7a
binwick <binwick@gmail.com>
2019-08-03 22:24:43 +0800

    20

---
+++ lc_0020/is_valid.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func isValid(s string) bool {
+	var stack []string
+	var parenMap = map[string]string{
+		")": "(",
+		"]": "[",
+		"}": "{",
+	}
+	for _, e := range s {
+		c := string(e)
+		if _, ok := parenMap[c]; !ok {
+			stack = append(stack, c)
+		} else {
+			if len(stack) == 0 {
+				return false
+			}
+			// pop
+			pop := stack[len(stack)-1]
+			stack = stack[0 : len(stack)-1]
+			if parenMap[c] != pop {
+				return false
+			}
+		}
+	}
+	return len(stack) == 0
+}
+
+func main() {
+	valid := isValid("()")
+	fmt.Println(valid)
+	valid = isValid("(})")
+	fmt.Println(valid)
+	valid = isValid("({})")
+	fmt.Println(valid)
+	valid = isValid("()[]{}")
+	fmt.Println(valid)
+}
